16756980e6e3dcf5a54f287be9a19a126d449144
Null <oiapSM.Smo@gmail.com>
2020-10-15 23:10:34 -0500

    Terminada la intefaz para celulares

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+let textSend = document.getElementById('imprimirTexto');
+let numeroRandom = Math.floor(Math.random() * 100);
+class mensaje{
+    constructor(mensaje){
+        this.mensaje = mensaje;
+        this.nuevo()
+        this.limpiarFomulario();
+    }
+
+    limpiarFomulario  = () => {
+         document.getElementById('formulario').reset()
+    }
+
+    nuevo = () => {
+        let user = 'User' + numeroRandom
+    let gordo = user.bold()    
+    textSend.innerHTML += '<p class="imprimirTexto" id="imprimirTexto"><span class="gordo">'  + gordo + '</span> : ' + this.mensaje +  '\n </p> '
+    }
+}
+
+
+
+enviarMensaje = (text) => {
+      let persona = new mensaje(text)
+}
+
+
